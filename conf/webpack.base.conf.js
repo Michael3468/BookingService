b4aba8a6b965f307eb6ad499397350aa8dfc8e30
Mikhail <mixail.workmail@yandex.ru>
2020-09-08 11:24:16 +0400

    del excess semicolon

--- conf/webpack.base.conf.js
+++ conf/webpack.base.conf.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 const PATHS = {
   src: path.join(__dirname, '../src'),
