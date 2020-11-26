9e0873c22d24bfcbeb2717964d1a6d7e87a88faa
Mikhail <mixail.workmail@yandex.ru>
2020-11-26 16:05:19 +0400

    add onchange function to change values range

--- src/components/ui-kit/formelements/range-slider/range-slider.js
+++ src/components/ui-kit/formelements/range-slider/range-slider.js
@@ -6,9 +6,17 @@ $(function() {
   $(".js-range-slider").ionRangeSlider({
     type: "double",
     min: 0,
-    max: 1000,
-    from: 310,
-    to: 635
+    max: 15800,
+    from: 5000,
+    to: 10000
+  });
+
+  $(".js-range-slider").on('change', function(){
+    var valueFrom = $('.irs-from').text();
+    var valueTo   = $('.irs-to').text();
+    var valuesRange = `${valueFrom} ₽ - ${valueTo} ₽`;
+    
+    $('.range-slider__caption-right').text(valuesRange);
   });
   
 });
