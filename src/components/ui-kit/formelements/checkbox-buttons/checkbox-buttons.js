f2877a37ef333a31f4dfc789fe264d56ee06dbc8
Mikhail <mixail.workmail@yandex.ru>
2020-12-06 16:00:43 +0400

    add expandability to checkbox-buttons

--- src/components/ui-kit/formelements/checkbox-buttons/checkbox-buttons.js
+++ src/components/ui-kit/formelements/checkbox-buttons/checkbox-buttons.js
@@ -1,1 +1,13 @@
 import './checkbox-buttons.scss';
+
+
+var button = document.getElementById("expandable-checkbox-list-chkboxbtns-1");
+var expandableCheckboxList = button.nextElementSibling;
+
+button.onclick = function(){
+  if ( expandableCheckboxList.classList.contains('display-none') ) {
+    expandableCheckboxList.classList.remove('display-none');
+  } else {
+    expandableCheckboxList.classList.add('display-none');
+  }
+}
