3c6592be316b46f08e2227bea13f3e89b9518a4b
Mikhail <mixail.workmail@yandex.ru>
2020-12-06 19:54:04 +0400

    add rotate class to expand-more

--- src/components/ui-kit/formelements/checkbox-buttons/checkbox-buttons.js
+++ src/components/ui-kit/formelements/checkbox-buttons/checkbox-buttons.js
@@ -1,13 +1,38 @@
 import './checkbox-buttons.scss';
 
+{
 
-var button = document.getElementById("expandable-checkbox-list-chkboxbtns-1");
-var expandableCheckboxList = button.nextElementSibling;
+  let expandableCheckbox = document.getElementById("expandable-checkbox-list-chkboxbtns-1");
+  let checkboxOptions = expandableCheckbox.nextElementSibling;
+  let expandMore = expandableCheckbox.lastElementChild.children[1];
 
-button.onclick = function(){
-  if ( expandableCheckboxList.classList.contains('display-none') ) {
-    expandableCheckboxList.classList.remove('display-none');
-  } else {
-    expandableCheckboxList.classList.add('display-none');
+  console.dir(expandMore);
+
+  expandableCheckbox.onclick = function(){
+    if ( isCheckboxOptionsVisible() ) {
+      hideCheckboxOptions();
+    } else {
+      showCheckboxOptions();
+    }
   }
+
+  function isCheckboxOptionsVisible() {
+    let notVisible = checkboxOptions.classList.contains('display-none');
+    if ( notVisible ) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  function showCheckboxOptions() {
+    checkboxOptions.classList.remove('display-none');
+    expandMore.classList.add('expandable-checkbox-list__expand-more-rotate');
+  }
+
+  function hideCheckboxOptions() {
+    checkboxOptions.classList.add('display-none');
+    expandMore.classList.remove('expandable-checkbox-list__expand-more-rotate');
+  }
+
 }
