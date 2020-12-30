b3e46fe19b4743ac9e5cb7ac5cb4109d08b3acd2
Mikhail <mixail.workmail@yandex.ru>
2020-12-30 10:26:52 +0400

    show/hide dropdown on click

--- src/components/ui-kit/form-elements/dropdown-guests/dropdown-guests.js
+++ src/components/ui-kit/form-elements/dropdown-guests/dropdown-guests.js
@@ -1,2 +1,27 @@
 import './dropdown-guests.scss';
 
+
+let isDropdownGuests = document.getElementsByClassName('dropdown-guests-block');
+
+Array.from(isDropdownGuests).forEach(element => {
+  element.addEventListener("click", showHideDropdownMenu, false);
+});
+
+
+function showHideDropdownMenu(){
+
+  let dropdown = this;
+  let menu = this.querySelector('.dropdown-guests__menu');
+  let cleanButton = this.querySelector('.dropdown-guests-button_clean');
+
+  menu.classList.toggle('visibility-hidden');
+  if ( menu.classList.contains('visibility-hidden') ) {
+    dropdown.classList.remove('menu-open');
+    cleanButton.classList.remove('dropdown-guests-button_clean_show');
+  } else {
+    dropdown.classList.add('menu-open');
+    cleanButton.classList.add('dropdown-guests-button_clean_show');
+  }
+
+}
+
