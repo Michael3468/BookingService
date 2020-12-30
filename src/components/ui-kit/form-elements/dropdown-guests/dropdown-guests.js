da9a92e9eaa7657b202f7a89ea6c951b0187a693
Mikhail <mixail.workmail@yandex.ru>
2020-12-30 15:39:42 +0400

    add increment, decrement buttons logic

--- src/components/ui-kit/form-elements/dropdown-guests/dropdown-guests.js
+++ src/components/ui-kit/form-elements/dropdown-guests/dropdown-guests.js
@@ -1,14 +1,48 @@
 import './dropdown-guests.scss';
 
 
-let isDropdownGuests = document.getElementsByClassName('dropdown-guests-block');
-
-Array.from(isDropdownGuests).forEach(element => {
-  element.addEventListener("click", showHideDropdownMenu, false);
-});
+addClickListenersToClasses('dropdown-guests-block', showHideDropdownMenu);
+addClickListenersToClasses('button-increment', buttonIncrementClick);
+addClickListenersToClasses('button-decrement', buttonDecrementClick);
 
 
-function showHideDropdownMenu(){
+function buttonDecrementClick() {
+
+  let buttonDecrement = this;
+  let counter = this.nextElementSibling;
+
+  if ( +(counter.innerText) > 0 ) {
+    counter.innerText = +(counter.innerText) - 1;
+    if ( +(counter.innerText) == 0 ) {
+      buttonDecrement.classList.remove('button-decrement_dark');
+    }
+  }
+
+}
+
+
+function buttonIncrementClick() {
+
+  let counter = this.previousElementSibling;
+  counter.innerText = +(counter.innerText) + 1;
+
+  let buttonDecrement = counter.previousElementSibling;
+  buttonDecrement.classList.add('button-decrement_dark');
+
+}
+
+
+function addClickListenersToClasses(className, functionName) {
+  let classNamesArr = document.getElementsByClassName(className);
+
+  Array.from(classNamesArr).forEach(element => {
+    element.addEventListener("click", functionName, false);
+  });
+
+}
+
+
+function showHideDropdownMenu() {
 
   let dropdown = this;
   let menu = this.querySelector('.dropdown-guests__menu');
