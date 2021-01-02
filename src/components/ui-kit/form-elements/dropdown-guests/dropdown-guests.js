dd1a019b7fc981d9282cb1c8124b171afc547859
Mikhail <mixail.workmail@yandex.ru>
2021-01-02 19:07:19 +0400

    add +/- buttons and clean button logic for counters

--- src/components/ui-kit/form-elements/dropdown-guests/dropdown-guests.js
+++ src/components/ui-kit/form-elements/dropdown-guests/dropdown-guests.js
@@ -4,12 +4,32 @@ import './dropdown-guests.scss';
 addClickListenersToClasses('dropdown-guests-block', showHideDropdownMenu);
 addClickListenersToClasses('button-increment', buttonIncrementClick);
 addClickListenersToClasses('button-decrement', buttonDecrementClick);
+addClickListenersToClasses('dropdown-guests-button_clean', cleanButtonClick);
 
 
-function buttonDecrementClick() {
+function cleanButtonClick(event) {
 
-  let buttonDecrement = this;
-  let counter = this.nextElementSibling;
+  const menu = this.offsetParent;
+  const menuCounters = menu.querySelectorAll('.counter');
+  const decrementButtons = menu.querySelectorAll('.button-decrement');
+
+  menuCounters.forEach(element => {
+    element.innerText = 0;
+  });
+  decrementButtons.forEach(element => {
+    element.classList.remove('button-decrement_dark');
+  });
+
+  this.classList.remove('dropdown-guests-button_clean_show');
+  event.stopPropagation();
+
+}
+
+
+function buttonDecrementClick(event) {
+
+  const buttonDecrement = this;
+  const counter = this.nextElementSibling;
 
   if ( +(counter.innerText) > 0 ) {
     counter.innerText = +(counter.innerText) - 1;
@@ -18,22 +38,32 @@ function buttonDecrementClick() {
     }
   }
 
+  const dropdown = event.target.offsetParent;
+  showHideCleanButton(dropdown);
+
+  event.stopPropagation();
+
 }
 
 
-function buttonIncrementClick() {
+function buttonIncrementClick(event) {
 
-  let counter = this.previousElementSibling;
+  const counter = this.previousElementSibling;
   counter.innerText = +(counter.innerText) + 1;
 
-  let buttonDecrement = counter.previousElementSibling;
+  const buttonDecrement = counter.previousElementSibling;
   buttonDecrement.classList.add('button-decrement_dark');
+
+  const dropdown = event.target.offsetParent;
+  showHideCleanButton(dropdown);
+
+  event.stopPropagation();
 
 }
 
 
 function addClickListenersToClasses(className, functionName) {
-  let classNamesArr = document.getElementsByClassName(className);
+  const classNamesArr = document.getElementsByClassName(className);
 
   Array.from(classNamesArr).forEach(element => {
     element.addEventListener("click", functionName, false);
@@ -42,20 +72,44 @@ function addClickListenersToClasses(className, functionName) {
 }
 
 
-function showHideDropdownMenu() {
+function showHideDropdownMenu(event) {
 
-  let dropdown = this;
-  let menu = this.querySelector('.dropdown-guests__menu');
-  let cleanButton = this.querySelector('.dropdown-guests-button_clean');
+  const dropdown = this;
+  const menu = this.querySelector('.dropdown-guests__menu');
+  const cleanButton = this.querySelector('.dropdown-guests-button_clean');
 
   menu.classList.toggle('visibility-hidden');
   if ( menu.classList.contains('visibility-hidden') ) {
+    //- hide menu
     dropdown.classList.remove('menu-open');
     cleanButton.classList.remove('dropdown-guests-button_clean_show');
   } else {
+    //- show menu
     dropdown.classList.add('menu-open');
-    cleanButton.classList.add('dropdown-guests-button_clean_show');
+
+    showHideCleanButton(dropdown);
+
   }
 
 }
 
+
+function showHideCleanButton(dropdown) {
+
+  const counters = dropdown.querySelectorAll('.counter');
+
+  let countersValSum = 0;
+  counters.forEach(element => {
+    countersValSum += +(element.innerText);
+  });
+
+  const cleanButton = dropdown.querySelector('.dropdown-guests-button_clean');
+  if ( countersValSum > 0 ) {
+    //- show clean button
+    cleanButton.classList.add('dropdown-guests-button_clean_show');
+  } else {
+    //- hide clean button
+    cleanButton.classList.remove('dropdown-guests-button_clean_show');
+  }
+
+}
