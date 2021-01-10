dbd995e3a6f01618d0644fd2ff69ab0ef3a9e49a
Mikhail <mixail.workmail@yandex.ru>
2021-01-10 20:10:16 +0400

    add filter-date-dropdown logic

--- src/components/ui-kit/form-elements/filter-date-dropdown/filter-date-dropdown.js
+++ src/components/ui-kit/form-elements/filter-date-dropdown/filter-date-dropdown.js
@@ -1,1 +1,17 @@
 import './filter-date-dropdown.scss';
+import '../../form-elements/filter-date-picker/filter-date-picker.js';
+
+
+$(function() {
+
+  let $isFilterDateDropdown = $('.filter-date-dropdown__input');
+  if ( $isFilterDateDropdown ) {
+    $isFilterDateDropdown.on('click', showHideDatePicker);
+  }
+
+});
+
+function showHideDatePicker(){
+  let $filterDateDropdown = $(this.offsetParent.nextElementSibling);
+  $filterDateDropdown.toggle('.dropdown-date__date-picker_hidden');
+};
