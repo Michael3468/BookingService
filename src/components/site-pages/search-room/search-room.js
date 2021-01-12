2cddc0297164ad13ae360abc0901c674127a3fcf
Mikhail <mixail.workmail@yandex.ru>
2021-01-12 16:13:53 +0400

    add options-column

--- src/components/site-pages/search-room/search-room.js
+++ src/components/site-pages/search-room/search-room.js
@@ -1,2 +1,5 @@
 import './search-room.scss';
 import '../../ui-kit/form-elements/filter-date-dropdown/filter-date-dropdown.js';
+import '../../ui-kit/form-elements/range-slider/range-slider.js';
+import '../../ui-kit/form-elements/dropdown-options/dropdown-options.js';
+import '../../ui-kit/form-elements/checkbox-buttons/checkbox-buttons.js';
