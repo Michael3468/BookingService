199b7d9c9a68a17637ef2322a801041af2a91821
Mikhail <mixail.workmail@yandex.ru>
2021-01-15 16:02:50 +0400

    add search-room page

--- src/components/site-pages/search-room/search-room.js
+++ src/components/site-pages/search-room/search-room.js
@@ -3,3 +3,4 @@ import '../../ui-kit/form-elements/filter-date-dropdown/filter-date-dropdown.js'
 import '../../ui-kit/form-elements/range-slider/range-slider.js';
 import '../../ui-kit/form-elements/dropdown-options/dropdown-options.js';
 import '../../ui-kit/form-elements/checkbox-buttons/checkbox-buttons.js';
+import '../../ui-kit/form-elements/pagination/pagination.js';
