ab62e61b11acc030d30044fd61f83129193c1c66
Mikhail <mixail.workmail@yandex.ru>
2021-03-06 17:36:57 +0400

    add calendar for all elements

--- src/components/ui-kit/cards/date-picker/date-picker.js
+++ src/components/ui-kit/cards/date-picker/date-picker.js
@@ -1,8 +1,7 @@
 import './date-picker.scss';
 import 'air-datepicker/dist/js/datepicker.min.js';
 
-
-$('#date-picker').datepicker({
+$('.date-picker').datepicker({
   range: true,
   multipleDatesSeparator: ' - ',
   clearButton: true,
@@ -11,30 +10,31 @@ $('#date-picker').datepicker({
   },
 
   onSelect: function (fd, d, picker) {
-    $("#startDate").val(fd.split(" - ")[0]);
-    $("#endDate").val(fd.split(" - ")[1]);
+    $('#startDate').val(fd.split(' - ')[0]);
+    $('#endDate').val(fd.split(' - ')[1]);
   },
+});
 
-})
-
-$(function(){
-
+$(function () {
   addApplyButton();
 
   let $applyButton = $('[data-action="apply"]');
   $applyButton.on('click', hideDatePicker);
-
 });
 
-
 function addApplyButton() {
-  let $isPickerButtons = $('#date-picker').find('.datepicker--buttons');
+  let $isPickerButtons = $('.date-picker').find('.datepicker--buttons');
   if ($isPickerButtons) {
-    $isPickerButtons.append('<span class="datepicker--button" data-action="apply">Применить</span>');
+    $isPickerButtons.append(
+      '<span class="datepicker--button" data-action="apply">Применить</span>'
+    );
   }
-};
+}
 
-function hideDatePicker(){
+function hideDatePicker() {
   let $isDatePicker = $('.dropdown-date__date-picker');
-  $isDatePicker.toggle('.dropdown-date__date-picker_hidden');
-};
+  // TODO удалить dropdown-date__date-picker_hidden везде
+  // $isDatePicker.toggle('.dropdown-date__date-picker_hidden');
+
+  $isDatePicker.css('display', 'none');
+}
