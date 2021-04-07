17fbd29fd2df4d386beb96e1f84079f2d103ddc4
Mikhail <mixail.workmail@yandex.ru>
2021-04-07 15:06:53 +0500

    prettier

--- src/components/ui-kit/form-elements/like-button/like-button.js
+++ src/components/ui-kit/form-elements/like-button/like-button.js
@@ -1,33 +1,27 @@
 import './like-button.scss';
 
-
-$(function(){
-
-  $('.like-button__input').on('click', function(event) {
-
+$(function () {
+  $('.like-button__input').on('click', function (event) {
     var element_id = event.target.id;
 
     toggleClasses(element_id);
     changeCounter(element_id);
-
   });
-
 });
 
-
 function toggleClasses(elementId) {
-  $('#icon_'+elementId).toggleClass('js-icon-checked-status');
-  $('#content_'+elementId).toggleClass('js-content-checked-status');
-  $('#container_'+elementId).toggleClass('js-container-checked-status');
+  $('#icon_' + elementId).toggleClass('js-icon-checked-status');
+  $('#content_' + elementId).toggleClass('js-content-checked-status');
+  $('#container_' + elementId).toggleClass('js-container-checked-status');
 }
 
 function changeCounter(elementId) {
-  var counterId = '#content_'+elementId;
-  var $counterValue = Number( $(counterId).text() );
+  var counterId = '#content_' + elementId;
+  var $counterValue = Number($(counterId).text());
 
-  if ( $(counterId).hasClass('js-content-checked-status') ) {
-    $(counterId).text($counterValue+1);
+  if ($(counterId).hasClass('js-content-checked-status')) {
+    $(counterId).text($counterValue + 1);
   } else {
-    $(counterId).text($counterValue-1);
-  };
+    $(counterId).text($counterValue - 1);
+  }
 }
