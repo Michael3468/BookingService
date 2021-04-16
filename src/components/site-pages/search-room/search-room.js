ca312995bc0043d9472a7cb4c0b169357c25dcb9
Mikhail <mixail.workmail@yandex.ru>
2021-04-16 15:11:42 +0500

    add responsive

--- src/components/site-pages/search-room/search-room.js
+++ src/components/site-pages/search-room/search-room.js
@@ -1,15 +1,33 @@
 import './search-room.scss';
 
-import '../../ui-kit/headers-and-footers/headers/headers.js';
-import '../../ui-kit/headers-and-footers/footers/footers.js';
+import '../../ui-kit/headers-and-footers/headers/headers';
+import '../../ui-kit/headers-and-footers/footers/footers';
 
-import '../../ui-kit/form-elements/filter-date-dropdown/filter-date-dropdown.js';
-import '../../ui-kit/form-elements/dropdown/_guests/dropdown-guests.js';
+import '../../ui-kit/form-elements/filter-date-dropdown/filter-date-dropdown';
+import '../../ui-kit/form-elements/dropdown/_guests/dropdown-guests';
 
-import '../../ui-kit/form-elements/range-slider/range-slider.js';
-import '../../ui-kit/form-elements/dropdown/_options/dropdown-options.js';
-import '../../ui-kit/form-elements/checkbox-buttons/checkbox-buttons.js';
-import '../../ui-kit/form-elements/rich-checkbox-buttons/rich-checkbox-buttons.js';
+import '../../ui-kit/form-elements/range-slider/range-slider';
+import '../../ui-kit/form-elements/dropdown/_options/dropdown-options';
+import '../../ui-kit/form-elements/checkbox-buttons/checkbox-buttons';
+import '../../ui-kit/form-elements/rich-checkbox-buttons/rich-checkbox-buttons';
 
-import '../../ui-kit/cards/room-card/room-card.js';
-import '../../ui-kit/form-elements/pagination/pagination.js';
+import '../../ui-kit/cards/room-card/room-card';
+import '../../ui-kit/form-elements/pagination/pagination';
+
+const optionsColumnButton = document.querySelector('.search-room__options-column_button');
+const menu = document.querySelector('.search-room__options-column');
+
+function showColumnOptionsMenu() {
+  const menuStatus = menu.style.display;
+  if (menuStatus !== 'block') {
+    menu.style.display = 'block';
+    menu.style.marginRight = '10px';
+    optionsColumnButton.style.rotate = '180deg';
+  } else {
+    menu.style.display = 'none';
+    menu.style.marginRight = '0px';
+    optionsColumnButton.style.rotate = '0deg';
+  }
+}
+
+optionsColumnButton.addEventListener('click', showColumnOptionsMenu);
