e0cd0fd18f7bb0ec46f23eba33994debf0eb4e2f
Mikhail <mixail.workmail@yandex.ru>
2021-05-06 17:42:47 +0500

    add slick-carousel

--- src/components/ui-kit/cards/room-card/room-card.js
+++ src/components/ui-kit/cards/room-card/room-card.js
@@ -1,5 +1,16 @@
+/* eslint-disable no-undef */
+/* eslint-disable prefer-arrow-callback */
 import './room-card.scss';
 
 import '../booking/__caption/booking__caption';
 import '../../form-elements/rate-button/rate-button';
 import './__feedbacks/__feedbacks';
+
+// slick-carousel
+import 'slick-carousel/slick/slick.min';
+
+// eslint-disable-next-line func-names
+$(function () {
+  $('.room-card__picture-block').slick();
+});
+// slick-carousel end
