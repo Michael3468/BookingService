f1872e1e26467ad79da31e29ba01533b6d08d202
Mixail <mixail.workmail@yandex.ru>
2021-12-31 14:02:59 +0300

    ref: add open/close header' dropdown by click

--- src/components/ui-kit/headers-and-footers/headers/headers.js
+++ src/components/ui-kit/headers-and-footers/headers/headers.js
@@ -2,3 +2,29 @@ import './headers.scss';
 
 import './header_logo/header_logo';
 import '../../form-elements/buttons/buttons';
+
+function getDropdownMenu(PointerEvent) {
+  const { currentTarget } = PointerEvent;
+  return currentTarget.querySelector('.js-header__nav-dropdown-content');
+}
+
+function handleHeaderDropdownClick(PointerEvent) {
+  getDropdownMenu(PointerEvent).classList.toggle('hidden');
+}
+
+function handleHeaderDropdownMouseLeave(PointerEvent) {
+  getDropdownMenu(PointerEvent).classList.add('hidden');
+}
+
+function headersDropdownShowHide() {
+  const dropdowns = document.querySelectorAll('.js-header__nav-dropdown');
+
+  if (dropdowns !== undefined) {
+    dropdowns.forEach((dropdown) => {
+      dropdown.addEventListener('click', handleHeaderDropdownClick);
+      dropdown.addEventListener('mouseleave', handleHeaderDropdownMouseLeave);
+    });
+  }
+}
+
+document.addEventListener('DOMContentLoaded', headersDropdownShowHide);
