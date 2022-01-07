67e761e24fd6bb4fe7e99df2f05845832cff1c9b
Mixail <mixail.workmail@yandex.ru>
2022-01-07 15:51:26 +0300

    ref: change open/close expand menu from checkbox to js

--- src/components/ui-kit/headers-and-footers/headers/headers.js
+++ src/components/ui-kit/headers-and-footers/headers/headers.js
@@ -3,6 +3,7 @@ import './headers.scss';
 import './header_logo/header_logo';
 import '../../form-elements/buttons/buttons';
 
+/* dropdown menu */
 function getDropdownMenu(PointerEvent) {
   const { currentTarget } = PointerEvent;
   return currentTarget.querySelector('.js-header__nav-dropdown-content');
@@ -28,3 +29,30 @@ function headersDropdownShowHide() {
 }
 
 document.addEventListener('DOMContentLoaded', headersDropdownShowHide);
+/* dropdown menu end */
+
+/* header-burger expand menu */
+const headerBurger = document.querySelector('.js-header-burger');
+const headerNav = document.querySelector('.js-header__nav');
+
+function toggleHeaderNav() {
+  headerNav.classList.toggle('hidden');
+}
+
+function addRemoveHiddenClass(element) {
+  if (window.innerWidth < 1025) {
+    element.classList.add('hidden');
+  } else {
+    element.classList.remove('hidden');
+  }
+}
+
+headerBurger.addEventListener('click', toggleHeaderNav);
+
+document.addEventListener('DOMContentLoaded', () => {
+  addRemoveHiddenClass(headerNav);
+});
+window.addEventListener('resize', () => {
+  addRemoveHiddenClass(headerNav);
+});
+/* header-burger expand menu end */
