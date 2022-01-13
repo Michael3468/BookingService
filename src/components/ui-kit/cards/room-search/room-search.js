a03f48b1664ec7d1e5a1cbb7c3f126d2bbd38097
Mikhail <mixail.workmail@yandex.ru>
2022-01-13 13:45:42 +0300

    fix: room-search form & submit button

--- src/components/ui-kit/cards/room-search/room-search.js
+++ src/components/ui-kit/cards/room-search/room-search.js
@@ -3,3 +3,10 @@ import '../card-background-border.scss';
 import '../../form-elements/dropdown/_options/dropdown-options.scss';
 
 import '../../form-elements/dropdown/_guests/dropdown-guests';
+
+const roomSearchForm = document.querySelector('.js-room-search-form');
+const roomSearchButton = document.querySelector('.js-room-search-button');
+
+roomSearchButton.addEventListener('click', () => {
+  roomSearchForm.submit();
+});
