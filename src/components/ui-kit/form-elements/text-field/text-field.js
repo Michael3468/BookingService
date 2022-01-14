23fe7daac784e6ac944a03b1c32f9110a21f858f
Mikhail <mixail.workmail@yandex.ru>
2022-01-14 13:54:57 +0300

    fix: addEventListener with null element

--- src/components/ui-kit/form-elements/text-field/text-field.js
+++ src/components/ui-kit/form-elements/text-field/text-field.js
@@ -3,12 +3,14 @@ import './text-field.scss';
 const subscriptionForm = document.querySelector('.js-subscription-form');
 const subscriptionSubmitButton = document.querySelector('.js-arrow-forward');
 
-subscriptionSubmitButton.addEventListener('click', () => {
-  subscriptionForm.submit();
-});
-
-subscriptionSubmitButton.addEventListener('keypress', (e) => {
-  if (e.code === 'Enter') {
+if (subscriptionSubmitButton) {
+  subscriptionSubmitButton.addEventListener('click', () => {
     subscriptionForm.submit();
-  }
-});
+  });
+
+  subscriptionSubmitButton.addEventListener('keypress', (e) => {
+    if (e.code === 'Enter') {
+      subscriptionForm.submit();
+    }
+  });
+}
