7ae579835fd003a52a2b52577e911a6b51be1021
Mikhail <mixail.workmail@yandex.ru>
2022-01-14 09:58:13 +0300

    fix: login form submit button

--- src/components/ui-kit/cards/login/login.js
+++ src/components/ui-kit/cards/login/login.js
@@ -1,2 +1,9 @@
 import './login.scss';
 import '../card-background-border.scss';
+
+const loginForm = document.querySelector('.js-login-form');
+const loginSubmitButton = document.querySelector('.js-login-submit-button');
+
+loginSubmitButton.addEventListener('click', () => {
+  loginForm.submit();
+});
