83b16f3af21b03fa53b7ef8bf6862992d92768fe
Mikhail <mixail.workmail@yandex.ru>
2022-01-14 09:46:18 +0300

    fix: submit button and add tabindex

--- src/components/ui-kit/cards/registration/registration.js
+++ src/components/ui-kit/cards/registration/registration.js
@@ -5,3 +5,10 @@ import '../../form-elements/text-field/text-field';
 import '../../form-elements/radio-buttons/radio-buttons';
 import '../../form-elements/masked-text-field/masked-text-field';
 import '../../form-elements/toggle/toggle';
+
+const registrationSubmitButton = document.querySelector('.js-registration-submit-button');
+const registrationForm = document.querySelector('.js-registration-form');
+
+registrationSubmitButton.addEventListener('click', () => {
+  registrationForm.submit();
+});
--- src/components/ui-kit/form-elements/toggle/toggle.js
+++ src/components/ui-kit/form-elements/toggle/toggle.js
@@ -1,1 +1,13 @@
 import './toggle.scss';
+
+const toggleButtons = document.querySelectorAll('.js-toggle__button');
+
+toggleButtons.forEach((item) => {
+  item.addEventListener('keypress', (e) => {
+    if (e.code === 'Enter') {
+      e.preventDefault();
+      // eslint-disable-next-line no-param-reassign
+      item.checked = !item.checked;
+    }
+  });
+});
