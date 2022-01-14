851575669005e7b8659410d7a29c93fdb8b28513
Mikhail <mixail.workmail@yandex.ru>
2022-01-14 13:27:55 +0300

    ref: add favicons

--- src/index.js
+++ src/index.js
@@ -1,25 +1,5 @@
 import './components/ui-kit/index/index';
-// import './assets/js/common';
+import './static/favicons';
+
 import './components/ui-kit/logo/logo';
 import './assets/scss/main.scss';
-
-// Bootstrap (example)
-// import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
-// import 'bootstrap/dist/css/bootstrap.min.css'
-
-// //requireAll block
-// function requireAll(requireContext) {
-//   return requireContext.keys().map(requireContext);
-// }
-
-// requireAll(require.context('./components', true, /^\.\/(?!.*(?:__tests__)).*\.(jsx?)$/));  // pattern to take each .js(x) files except of the ones with __tests__ directory https://regex101.com/r/J8NWTj/1
-// requireAll(require.context('./pages', true, /^\.\/(?!.*(?:__tests__)).*\.(jsx?)$/));
-// //requireAll block end
-
-/* jquery test
-
-$(function() {
-  $('body').css('background-color','orange');
-});
-
-*/
