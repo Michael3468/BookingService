a2f633d3f31a75c1ed098d981c2bc61550d41705
Mikhail <mixail.workmail@yandex.ru>
2022-01-20 06:40:19 +0300

    fix: login form

--- src/components/ui-kit/cards/login/login.js
+++ src/components/ui-kit/cards/login/login.js
@@ -1,9 +1,2 @@
 import './login.scss';
 import '../card-background-border.scss';
-
-const loginForm = document.querySelector('.js-login-form');
-const loginSubmitButton = document.querySelector('.js-login-submit-button');
-
-loginSubmitButton.addEventListener('click', () => {
-  loginForm.submit();
-});
