edb60eead4007cb5d5241975579c1793ff0b67fb
Mikhail <mixail.workmail@yandex.ru>
2022-01-20 06:32:39 +0300

    fix: booking form

--- src/components/ui-kit/cards/booking/booking.js
+++ src/components/ui-kit/cards/booking/booking.js
@@ -6,10 +6,3 @@ import './__caption/booking__caption';
 import '../../form-elements/dropdown-date/dropdown-date';
 import '../../form-elements/dropdown/_guests/dropdown-guests';
 import '../../form-elements/buttons/buttons';
-
-const bookingForm = document.querySelector('.js-booking-form');
-const bookingSubmitButton = document.querySelector('.js-booking-submit-button');
-
-bookingSubmitButton.addEventListener('click', () => {
-  bookingForm.submit();
-});
