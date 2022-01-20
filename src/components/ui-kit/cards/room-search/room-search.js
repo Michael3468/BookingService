bc9472eb46a321d4874f94208e068f59ce18bc29
Mikhail <mixail.workmail@yandex.ru>
2022-01-20 06:06:14 +0300

    fix: room-search form

--- src/components/ui-kit/cards/room-search/room-search.js
+++ src/components/ui-kit/cards/room-search/room-search.js
@@ -3,10 +3,3 @@ import '../card-background-border.scss';
 import '../../form-elements/dropdown/_options/dropdown-options.scss';
 
 import '../../form-elements/dropdown/_guests/dropdown-guests';
-
-const roomSearchForm = document.querySelector('.js-room-search-form');
-const roomSearchButton = document.querySelector('.js-room-search-button');
-
-roomSearchButton.addEventListener('click', () => {
-  roomSearchForm.submit();
-});
