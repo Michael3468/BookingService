3d44f3b50b4285d18761c7467658b2e86052f27b
Mikhail <mixail.workmail@yandex.ru>
2022-01-20 06:18:54 +0300

    fix: registration form

--- src/components/ui-kit/cards/registration/registration.js
+++ src/components/ui-kit/cards/registration/registration.js
@@ -5,10 +5,3 @@ import '../../form-elements/text-field/text-field';
 import '../../form-elements/radio-buttons/radio-buttons';
 import '../../form-elements/masked-text-field/masked-text-field';
 import '../../form-elements/toggle/toggle';
-
-const registrationSubmitButton = document.querySelector('.js-registration-submit-button');
-const registrationForm = document.querySelector('.js-registration-form');
-
-registrationSubmitButton.addEventListener('click', () => {
-  registrationForm.submit();
-});
