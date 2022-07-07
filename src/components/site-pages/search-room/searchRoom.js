454ee812be7b4fd27361601ec6e23eda56186679
Mikhail <mixail.workmail@yandex.ru>
2022-07-07 09:37:56 +0300

    refactor: add FilterDatePicker and FilterDateDropdown classes (#45)

--- src/components/site-pages/search-room/searchRoom.js
+++ src/components/site-pages/search-room/searchRoom.js
@@ -1,3 +1,5 @@
+import FilterDateDropdown from '../../ui-kit/form-elements/filter-date-dropdown/filterDateDropdown';
+
 import './search-room.scss';
 
 import '../../ui-kit/cards/room-card/roomCard';
@@ -6,13 +8,15 @@ import '../../ui-kit/form-elements/checkbox-buttons/checkboxButtons';
 import '../../ui-kit/form-elements/custom-checkbox/customCheckbox';
 import '../../ui-kit/form-elements/dropdown/_guests/dropdownGuests';
 import '../../ui-kit/form-elements/dropdown/_options/dropdownOptions';
-import '../../ui-kit/form-elements/filter-date-dropdown/filterDateDropdown';
 import '../../ui-kit/form-elements/pagination/pagination';
 import '../../ui-kit/form-elements/range-slider/rangeSlider';
 import '../../ui-kit/form-elements/rich-checkbox-buttons/richCheckboxButtons';
 
 import '../../ui-kit/headers-and-footers/header/header';
 import '../../ui-kit/headers-and-footers/footer/footer';
+
+// eslint-disable-next-line no-new
+new FilterDateDropdown();
 
 const optionsColumnButton = document.querySelector('.js-search-room-options-column-button');
 const menu = document.querySelector('.js-search-room-options-column');
--- src/components/ui-kit/form-elements/filter-date-dropdown/filterDateDropdown.js
+++ src/components/ui-kit/form-elements/filter-date-dropdown/filterDateDropdown.js
@@ -1,29 +1,42 @@
-import '../filter-date-picker/filterDatePicker';
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-underscore-dangle */
+import FilterDatePicker from '../filter-date-picker/filterDatePicker';
 
 import './filter-date-dropdown.scss';
 
-function showHideDatePicker(event) {
-  const filterDateDropdown = event.target.closest('.js-filter-date-dropdown')
-    .querySelector('.js-dropdown-date__date-picker');
-  const filterStatus = filterDateDropdown.style.display;
+// eslint-disable-next-line no-new
+new FilterDatePicker();
 
-  if (filterStatus === 'block') {
-    filterDateDropdown.style.display = 'none';
-  } else {
-    filterDateDropdown.style.display = 'block';
+class FilterDateDropdown {
+  constructor() {
+    this._addListeners();
+  }
+
+  _showHideDatePicker(event) {
+    const filterDateDropdown = event.target.closest('.js-filter-date-dropdown')
+      .querySelector('.js-dropdown-date__date-picker');
+    const filterStatus = filterDateDropdown.style.display;
+
+    if (filterStatus === 'block') {
+      filterDateDropdown.style.display = 'none';
+    } else {
+      filterDateDropdown.style.display = 'block';
+    }
+  }
+
+  _handleFilterDateDropdownKeyPress(e) {
+    if (e.code === 'Enter') {
+      this._showHideDatePicker(e);
+    }
+  }
+
+  _addListeners() {
+    const filterDateDropdown = document.querySelector('.js-filter-date-dropdown__input');
+    filterDateDropdown.addEventListener('click', this._showHideDatePicker);
+    filterDateDropdown.addEventListener('keypress', (e) => {
+      this._handleFilterDateDropdownKeyPress(e);
+    });
   }
 }
 
-function handleFilterDateDropdownKeyPress(e) {
-  if (e.code === 'Enter') {
-    showHideDatePicker(e);
-  }
-}
-
-(() => {
-  const filterDateDropdown = document.querySelector('.js-filter-date-dropdown__input');
-  filterDateDropdown.addEventListener('click', showHideDatePicker);
-  filterDateDropdown.addEventListener('keypress', (e) => {
-    handleFilterDateDropdownKeyPress(e);
-  });
-})();
+export default FilterDateDropdown;
