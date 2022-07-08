4f93aba33ad4ff199159a49974ce08132204596d
Mikhail <mixail.workmail@yandex.ru>
2022-07-08 11:38:03 +0300

    refactor: add Toggle class (#45)

--- src/components/ui-kit/cards/registration/registration.js
+++ src/components/ui-kit/cards/registration/registration.js
@@ -1,11 +1,13 @@
 /* eslint-disable no-new */
 import MaskedTextField from '../../form-elements/masked-text-field/maskedTextField';
+import Toggle from '../../form-elements/toggle/toggle';
 
 import '../../form-elements/radio-buttons/radioButtons';
 import '../../form-elements/text-field/textField';
-import '../../form-elements/toggle/toggle';
 
 import './registration.scss';
 import '../card-background-border.scss';
 
 new MaskedTextField();
+
+new Toggle();
