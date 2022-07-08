bb56666e8bc16570b834278658b852731d7e302d
Mikhail <mixail.workmail@yandex.ru>
2022-07-08 08:28:29 +0300

    refactor: add MaskedTextField class (#45)

--- src/components/ui-kit/cards/registration/registration.js
+++ src/components/ui-kit/cards/registration/registration.js
@@ -1,7 +1,11 @@
+/* eslint-disable no-new */
+import MaskedTextField from '../../form-elements/masked-text-field/maskedTextField';
+
 import '../../form-elements/radio-buttons/radioButtons';
-import '../../form-elements/masked-text-field/maskedTextField';
 import '../../form-elements/text-field/textField';
 import '../../form-elements/toggle/toggle';
 
 import './registration.scss';
 import '../card-background-border.scss';
+
+new MaskedTextField();
--- src/components/ui-kit/form-elements/masked-text-field/maskedTextField.js
+++ src/components/ui-kit/form-elements/masked-text-field/maskedTextField.js
@@ -1,14 +1,26 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-underscore-dangle */
 import Inputmask from 'inputmask';
 
 import './masked-text-field.scss';
 
-window.addEventListener('DOMContentLoaded', () => {
-  const maskedDateField = document.querySelector(
-    '.js-masked-text-field__input',
-  );
-  Inputmask({
-    alias: 'datetime',
-    inputFormat: 'dd.mm.yyyy',
-    placeholder: 'дд.мм.гггг',
-  }).mask(maskedDateField);
-});
+class MaskedTextField {
+  constructor() {
+    this.elem = this._initMaskedTextField();
+  }
+
+  _initMaskedTextField() {
+    window.addEventListener('DOMContentLoaded', () => {
+      const maskedDateField = document.querySelector(
+        '.js-masked-text-field__input',
+      );
+      Inputmask({
+        alias: 'datetime',
+        inputFormat: 'dd.mm.yyyy',
+        placeholder: 'дд.мм.гггг',
+      }).mask(maskedDateField);
+    });
+  }
+}
+
+export default MaskedTextField;
