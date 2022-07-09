1f558f9cf46db30a1c5c549ac5710ff655bef446
Mikhail <mixail.workmail@yandex.ru>
2022-07-09 14:47:43 +0300

    chore: add rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,14 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base'],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
   },
   rules: {
+    'no-underscore-dangle': 'off',
+    'class-methods-use-this': 'off',
+    'no-new': 'off',
   },
 };
