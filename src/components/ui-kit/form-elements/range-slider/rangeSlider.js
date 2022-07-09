50f77298d7bd7f278acb4dfb66ab151cc098d5f1
Mikhail <mixail.workmail@yandex.ru>
2022-07-09 10:01:53 +0300

    fix: range-slider visibility and values

--- src/components/ui-kit/form-elements/range-slider/rangeSlider.js
+++ src/components/ui-kit/form-elements/range-slider/rangeSlider.js
@@ -1,34 +1,22 @@
-/* eslint-disable class-methods-use-this */
-/* eslint-disable no-underscore-dangle */
 /* eslint-disable no-undef */
 import 'ion-rangeslider/js/ion.rangeSlider.min';
 
 import './range-slider.scss';
 
-class RangeSlider {
-  constructor() {
-    this.elem = this._initRangeSlider();
-  }
+(() => {
+  $('.js-range-slider').ionRangeSlider({
+    type: 'double',
+    min: 0,
+    max: 15800,
+    from: 5000,
+    to: 10000,
+  });
 
-  _initRangeSlider() {
-    return () => {
-      $('.js-range-slider').ionRangeSlider({
-        type: 'double',
-        min: 0,
-        max: 15800,
-        from: 5000,
-        to: 10000,
-      });
+  $('.js-range-slider').on('change', () => {
+    const $valueFrom = $('.irs-from').text();
+    const $valueTo = $('.irs-to').text();
+    const $valuesRange = `${$valueFrom} ₽ - ${$valueTo} ₽`;
 
-      $('.js-range-slider').on('change', () => {
-        const $valueFrom = $('.irs-from').text();
-        const $valueTo = $('.irs-to').text();
-        const $valuesRange = `${$valueFrom} ₽ - ${$valueTo} ₽`;
-
-        $('.range-slider__caption_right').text($valuesRange);
-      });
-    };
-  }
-}
-
-export default RangeSlider;
+    $('.js-range-slider__caption-right').text($valuesRange);
+  });
+})();
