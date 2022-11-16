0c5c54746208222dfc6072bd8d60773540c6d5c5
Mikhail <mixail.workmail@yandex.ru>
2022-11-16 12:00:10 +0300

    move feedbacks to upper directory (#51)

--- src/components/ui-kit/cards/room-card/roomCard.js
+++ src/components/ui-kit/cards/room-card/roomCard.js
@@ -1,6 +1,6 @@
-import './__feedbacks/__feedbacks';
 import './slick-carousel/slickCarousel';
 import '../booking/__caption/bookingCaption';
+import '../feedbacks/feedbacks';
 import '../../form-elements/rate-button/rateButton';
 
 import './room-card.scss';
